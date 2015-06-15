85ebf07c17ce2b9c8597b1acb3743e7d71c80bfe
David Huie <dahuie@gmail.com>
2015-06-15 15:25:05 -0700

    Added comments to satisfy golint

--- request.go
+++ request.go
@@ -2,11 +2,13 @@ package cloudfront
 
 import "fmt"
 
+// Request is a URL that will expire.
 type Request struct {
 	URL     string
 	Expires int64
 }
 
+// CannedPolicy is the default Cloudfront policy for expiring URLs.
 func (req *Request) CannedPolicy() string {
 	if req == nil {
 		return ""
